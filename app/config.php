7c55b31ca7548f85d66a613174d2e52bb2679f32
Goesticandra Wijaya <49840941+goesticandra@users.noreply.github.com>
2023-09-25 14:35:22 +0700

    Add files via upload

---
+++ app/config.php
@@ -0,0 +1,8 @@
+<?php
+//Config Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'ur96eg1n_idbuzzerp');
+define('DB_PASS', 'Instagram1');
+define('DB_NAME', 'ur96eg1n_idbuzzerp');
+define('TIMEZONE', 'Asia/Jakarta');
+define('ENCRYPTION_KEY', 'c5fbb0694cb9c7e6dda5926478149672');
